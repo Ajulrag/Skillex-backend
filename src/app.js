cce24bf0eacd962c5679439ac257852e39596315
Ajulrag.b <ajulrag353@gmail.com>
2023-08-24 11:18:55 +0530

    feat:socet io implementation

--- src/app.js
+++ src/app.js
@@ -27,7 +27,8 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors({
-    origin: REACT
+    origin: REACT,
+    credentials:true
 }))
 
 app.use(function (req, res, next) {
