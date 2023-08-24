ea0ed706053fc43859f1f737b6004e7171e5ab50
Ajulrag.b <ajulrag353@gmail.com>
2023-08-24 12:27:39 +0530

    testing cors

--- src/app.js
+++ src/app.js
@@ -26,17 +26,14 @@ only this line.dint need next line
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors({
-    origin: REACT,
-    credentials:true
-}))
+app.use(cors())
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', REACT);
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', REACT);
+//     res.header('Access-Control-Allow-Credentials', true);
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+// });
 
 app.use('/', indexRoutes);
 app.use('/admin', adminRoutes);
